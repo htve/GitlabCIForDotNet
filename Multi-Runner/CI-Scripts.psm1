a1d76fdac5d44526fb3ee63d45ee2f471d55755e
黄涛 <htve@outlook.com>
2019-04-03 15:41:37 +0800

    变更msbuild的获取方式

--- Multi-Runner/CI-Scripts.psm1
+++ Multi-Runner/CI-Scripts.psm1
@@ -4,7 +4,8 @@
 ## 作者:黄涛
 ## 站点:https://github.com/htve/GitlabCIForDotNet
 
-$Msbuild15Path = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\amd64\msbuild.exe'
+## MSBuild 的默认安装路径
+if(!$MsbuildPath) { $MsbuildPath = 'C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\amd64\MSBuild.exe' }
 
 function GetServers()
 {
@@ -51,47 +52,24 @@ function GetServerSession([string]$IP)
 
 function GetMsBuildPath([switch] $Use32BitMsBuild)
 {
-	## 判断是否存在MSBuild v15
-	if(Test-Path -Path $Msbuild15Path)
-	{
-		return $Msbuild15Path
-	}
-	
-    ## 获取MsBuild.exe的最新版本的路径。如果找不到MsBuild.exe，则抛出异常。
-	$registryPathToMsBuildToolsVersions = 'HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\'
-	if ($Use32BitMsBuild)
-	{
-		## 如果32位路径存在则使用它，否则将使用与当前系统位一致的路径。
-		$registryPathTo32BitMsBuildToolsVersions = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\MSBuild\ToolsVersions\'
-		if (Test-Path -Path $registryPathTo32BitMsBuildToolsVersions)
-		{
-			$registryPathToMsBuildToolsVersions = $registryPathTo32BitMsBuildToolsVersions
+	if($MsbuildPath -eq $null -or $MsbuildPath.Trim() -eq "") {
+		$item = Get-ChildItem -Path "C:\Program Files (x86)"  -Include MSBuild.exe -Recurse | Where-Object {$_.DirectoryName.EndsWith("Bin\amd64")}
+		$item = $item.VersionInfo | Sort-Object -Property FileVersion -Descending | Select-Object FileName -First 1
+		$MsbuildPath = $item.FileName
+		if($MsbuildPath) { 
+			Write-Host "Searching for MSBuild.exe takes a long time. It is recommended to write the path '$MsbuildPath' to the variable `$MsbuildPath." 
+		}
+		else {
+			throw "Search MSBuild.exe failed"
+			exit 1
 		}
 	}
+	
+	if($Use32BitMsBuild) { $MsbuildPath = $MsbuildPath -replace "\\amd64","" }
+	
+	if(!(Test-Path $MsbuildPath -PathType Leaf)) { throw "MsBuild.exe was not found on this system."; exit 1 }
 
-	## 获取MsBuild最新版本所在目录的路径。
-	$msBuildToolsVersionsStrings = Get-ChildItem -Path $registryPathToMsBuildToolsVersions | Where-Object { $_ -match '[0-9]+\.[0-9]' } | Select-Object -ExpandProperty PsChildName
-	$msBuildToolsVersions = @{}
-	$msBuildToolsVersionsStrings | ForEach-Object {$msBuildToolsVersions.Add($_ -as [double], $_)}
-	$largestMsBuildToolsVersion = ($msBuildToolsVersions.GetEnumerator() | Sort-Object -Descending -Property Name | Select-Object -First 1).Value
-	$registryPathToMsBuildToolsLatestVersion = Join-Path -Path $registryPathToMsBuildToolsVersions -ChildPath ("{0:n1}" -f $largestMsBuildToolsVersion)
-	$msBuildToolsVersionsKeyToUse = Get-Item -Path $registryPathToMsBuildToolsLatestVersion
-	$msBuildDirectoryPath = $msBuildToolsVersionsKeyToUse | Get-ItemProperty -Name 'MSBuildToolsPath' | Select -ExpandProperty 'MSBuildToolsPath'
-
-	if(!$msBuildDirectoryPath)
-	{
-		throw 'The registry on this system does not appear to contain the path to the MsBuild.exe directory.'
-	}
-
-	## 获取MsBuild可执行文件的路径。
-	$msBuildPath = (Join-Path -Path $msBuildDirectoryPath -ChildPath 'msbuild.exe')
-
-	if(!(Test-Path $msBuildPath -PathType Leaf))
-	{
-		throw "MsBuild.exe was not found on this system at the path specified in the registry, '$msBuildPath'."
-	}
-
-	return $msBuildPath
+	return $MsbuildPath
 }
 
 function TryDotnet()
@@ -131,38 +109,6 @@ function GetFile(
 		exit 1 
 	}
 	return $file
-}
-
-function GetVisualStudioToolsVersion
-{
-    ## vs版本列表
-    $vsCommandPromptPaths = @(
-		@{Path=$Msbuild15Path;Version="15.0";}
-        @{Path=$env:VS140COMNTOOLS + 'VsDevCmd.bat';Version="14.0";}
-        @{Path=$env:VS120COMNTOOLS + 'VsDevCmd.bat';Version="12.0";}
-        @{Path=$env:VS110COMNTOOLS + 'VsDevCmd.bat';Version="11.0";}
-        @{Path=$env:VS100COMNTOOLS + 'vcvarsall.bat';Version="10.0";}
-    )
-
-	$vsToolsVersion = $null
-	foreach ($path in $vsCommandPromptPaths)
-	{
-		try
-		{
-			if (Test-Path -Path $path.Path)
-			{
-				$vsToolsVersion ="/ToolsVersion:"+ $path.Version
-				break
-			}
-		}
-		catch 
-        { 
-            throw $_.Exception
-            exit 1
-        }
-	}
-    ## 返回 MsBuild ToolsVersion 参数
-	return $vsToolsVersion
 }
 
 function RestorePackages(
@@ -252,15 +198,13 @@ function InvokeMsBuild(
 {   
     $build= GetMsBuildPath -Use32BitMsBuild:$Use32BitMsBuild
     Write-Host "MsBuild Path: $build`n"
-     
-    $toolsVersion=GetVisualStudioToolsVersion
 
     Write-Host "Start Build ...`n"
 	if([String]::IsNullOrEmpty($PublishProfile)){
-      $result = ."$build" $projectPath $toolsVersion /p:RunCodeAnalysis=false /consoleloggerparameters:ErrorsOnly /p:Configuration=$Configuration /nologo /verbosity:quiet /maxcpucount 
+      $result = ."$build" $projectPath /p:RunCodeAnalysis=false /consoleloggerparameters:ErrorsOnly /p:Configuration=$Configuration /nologo /verbosity:quiet /maxcpucount 
 	}
 	else{
-	  $result = ."$build" $projectPath $toolsVersion /p:RunCodeAnalysis=false /consoleloggerparameters:ErrorsOnly /p:Configuration=$Configuration /nologo /verbosity:quiet /maxcpucount /p:DeployOnBuild=true /p:publishUrl=$global:CiProjectPath/DeployArtifacts /p:PublishProfile=$PublishProfile
+	  $result = ."$build" $projectPath /p:RunCodeAnalysis=false /consoleloggerparameters:ErrorsOnly /p:Configuration=$Configuration /nologo /verbosity:quiet /maxcpucount /p:DeployOnBuild=true /p:publishUrl=$global:CiProjectPath/DeployArtifacts /p:PublishProfile=$PublishProfile
 	}
 	if($result.length -gt 0)
 	{ 
